ae052df28e43f4678d7742b49499b60e103d0cf0
susm16 <sm.suma@gmail.com>
2017-08-02 18:06:38 +0530

    My first commit

---
+++ MathHelper/src/main/java/com/maven/math/MathHelper/Calculator.java
@@ -0,0 +1,24 @@
+package com.maven.math.MathHelper;
+
+/**
+ * Hello world!
+ *
+ */
+public class Calculator 
+{
+    public int add(int i, int j){
+    	return i+j;
+    }
+    
+    public int subtract(int i, int j){
+    	return i-j;
+    }
+    
+    public int multiple(int i, int j){
+    	return i-j;
+    }
+    
+    public int divide(int i, int j){
+    	return i/j;
+    }
+}
